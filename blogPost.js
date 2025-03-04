dab061ad5ccd8d09f208af183c56c700ebf7fa02
Talaku Bekele <talakubekele@Talakus-MacBook-Pro.local>
2025-03-03 20:58:28 -0500

    Initial Commit

---
+++ blogPost.js
@@ -0,0 +1,131 @@
+import express from "express";
+
+const app = express();
+const PORT = 3034;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const posts = [
+  {
+    id: 1,
+    title: "This is title for everyone I just suggested",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse, perferendis corrupti illum? Vero nemo illo labore cumque autem",
+    author: "by Me",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 2,
+    title: "second Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse, perferendis corrupti illum? Vero nemo illo labore cumque autem",
+    author: "by Famouse you",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 3,
+    title: "Third Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse",
+    author: "the third author",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 4,
+    title: "Forth Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse",
+    author: "the third author",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 5,
+    title: "Fivth Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse",
+    author: "the third author",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 6,
+    title: "Sixth Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse",
+    author: "the third author",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 7,
+    title: "Seventh Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse",
+    author: "the third author",
+    date: new Date().getFullYear(),
+  },
+  {
+    id: 8,
+    title: "eighth Title",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate, dolorem nemo dignissimos optio, pariatur consectetur ratione repellendus aspernatur placeat esse",
+    author: "the third author",
+    date: new Date().getFullYear(),
+  },
+];
+
+let lastId = 2;
+
+app.get("/posts", (req, res) => {
+  console.log(posts);
+  res.json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(400).json({ message: "this is not post" });
+  res.json(post);
+});
+
+app.post("/posts", (req, res) => {
+  const newId = (lastId += 1);
+  const post = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(post);
+  res.status(201).json(post);
+});
+
+app.patch("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+
+  if (!post)
+    return res.status(404).json({ message: "Post is now not available" });
+
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
+
+  res.json(post);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "try agian!" });
+
+  posts.splice(index, 1);
+  res.json({ message: " Post deleted" });
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`API is running at http://localhost:${PORT}`);
+});
---
+++ index.js
@@ -0,0 +1,86 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3033;
+const API_URL = "http://localhost:3034";
+
+app.use(express.static("Public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts`);
+    console.log(response);
+    res.render("index.ejs", { posts: response.data });
+  } catch (error) {
+    res.status(400).json({ message: "this is an Error" });
+  }
+});
+
+app.get("/new", (req, res) => {
+  res.render("blog.ejs",
+  {
+      header: "New Post",
+      submit: "Create One Now",
+  });
+});
+
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+
+    console.log(response.data);
+
+    res.render("blog.ejs",
+      {
+          header: "Edit Post",
+          submit: "Update Post",
+          post: response.data,
+      });
+         
+  } catch (error) {
+    res.status(400).json({ message: "This is greate I think" });
+  }
+
+});
+
+app.post("/api/posts", async (req, res) => {
+  try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(400).json({ message: "O! that is greate men!" });
+  }
+});
+
+app.post("/api/posts/:id", async (req, res) => {
+  console.log("called");
+  try {
+    const response = await axios.patch(
+      `${API_URL}/posts/${req.params.id}`,
+      req.body
+    );
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(400).json({ message: "Try Again Please..." });
+  }
+});
+
+app.get("/api/posts/delete/:id", async (req, res) => {
+  try {
+    await axios.delete(`${API_URL}/posts/${req.params.id}`);
+    res.redirect("/");
+  } catch (error) {
+    res.status(400).json({ message: "Error is Occured now" });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`URL is running at  http://localhost:${PORT}`);
+});
